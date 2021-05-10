6bf7fd8df6d27a633b0dde4638f444cccf577956
Dagbfatih <Dagbfatih@users.noreply.github.com>
2021-05-11 01:37:38 +0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
